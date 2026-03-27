6bfe0d2fb552e8a00ebcfd66542163f404d9f8d0
unknown <anderson.garcia-n@labinfo.is.escuelaing.edu.co>
2026-03-27 07:56:24 -0500

    Punto 1: Se cambio a que todas los path tuvieran esto /api/v1/blueprints

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintsAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintsAPIController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/blueprints")
+@RequestMapping("api/v1/blueprints") //Punto 1 del parcial
 public class BlueprintsAPIController {
 
     private final BlueprintsServices services;
